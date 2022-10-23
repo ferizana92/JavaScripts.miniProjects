457fd66b73ae618eb2d8f9efcd9b3f5751578b84
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-10-23 14:17:46 +0330

    exercise for logiccal operator

---
+++ logical operator/script.js
@@ -0,0 +1,29 @@
+const Number1 = prompt("please add the first number!");
+const Number2 = prompt("please add the second number!");
+console.log(Number1);
+
+const Number1TypeNumber= +Number1
+const Number2TypeNumber= +Number2
+console.log(Number1TypeNumber);
+
+const plusResult = Number1TypeNumber + Number2TypeNumber
+
+const minusResult = Number1TypeNumber - Number2TypeNumber
+
+const multipleResult = Number1TypeNumber * Number2TypeNumber
+
+const Result = Number1TypeNumber / Number2TypeNumber
+
+
+console.log(plusResult);
+console.log(minusResult);
+console.log(multipleResult);
+console.log(Result);
+
+
+alert (plusResult);
+alert (minusResult);
+alert (multipleResult);
+alert (Result);
+alert (Number1);
+alert (Number2);
