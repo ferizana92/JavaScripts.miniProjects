ba2d78eab5680e44d87be736fc14d1d04a879a03
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-10-24 15:11:22 +0330

    practice about odd or even numbers

---
+++ number/script2.js
@@ -0,0 +1,10 @@
+const userNumber = prompt("please enter your number!");
+alert(userNumber)
+if (userNumber % 2 === 0) {
+
+    alert("Your number is even.")
+
+} else {
+
+    alert("Your number is odd.")
+}
