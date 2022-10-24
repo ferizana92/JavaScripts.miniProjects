999f80606652d4689da78b6836a5ee1ba74ecae0
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-10-24 16:52:45 +0330

    practice about age2

--- age/script4.js
+++ age/script4.js
@@ -1,7 +1,18 @@
-const userAge = prompt("please enter your age!",0);
+const userAge = prompt("please enter your age!", 0);
 
 if (userAge < 18) {
-    alert( "you can not enter the site!")
-}else {
-    alert( "please register!")
+    alert("you can not enter the site!")
+} else {
+    alert("please register!")
+}
+// ==========
+
+const userBirthday = prompt("please enter your birthday!", 0);
+const realAge = 1401 - userBirthday
+if (isNaN(realAge)) {
+    alert("birthday is not valid!")
+
+} else {
+
+    alert("your age is:" + realAge)
 }
