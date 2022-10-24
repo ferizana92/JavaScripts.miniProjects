cdf65b041e900b0df9c05f9161aacc9ff7779a4a
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-10-24 15:32:45 +0330

    practice about Avarage

---
+++ avarage/script3.js
@@ -0,0 +1,7 @@
+const userThirdNumber = Number(prompt("please enter third number!"));
+const userFirstNumber =Number( prompt("please enter first number!"));
+const userSecondNumber = Number(prompt("please enter second number!"));
+
+const Avarage = (userFirstNumber + userSecondNumber + userThirdNumber) / 3
+
+alert(Avarage)
