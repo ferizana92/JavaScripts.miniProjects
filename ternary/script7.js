ddb3180573a0743351602a9c657a9890076d9b76
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-10-26 16:17:46 +0330

    practices about ternary

---
+++ ternary/script7.js
@@ -0,0 +1,9 @@
+const num1 = 3
+const num2 = 4
+// if(num1*num2===12){
+//     alert("yes that is right")
+// }else{
+//     alert("no that is wrong")
+// }
+// Ternary conditions:
+num1 * num2 === 12 ? alert("yes that is right") : alert("no that is wrong")
