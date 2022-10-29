8ec0f30948ac146cf34c7f044b6a988742493130
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-10-29 13:46:27 +0330

    Practice with function parameter

--- number/script2.js
+++ number/script2.js
@@ -8,3 +8,16 @@ if (userNumber % 2 === 0) {
 
     alert("Your number is odd.")
 }
+// =========function for being even or odd===========//
+function isOddorEven(number) {
+    if (number % 2 === 0) {
+
+        alert("Your number is even.")
+
+    } else {
+
+        alert("Your number is odd.")
+    }
+}
+isOddorEven(119)
+isOddorEven(118)
