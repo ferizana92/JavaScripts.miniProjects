269c060ef94a4d137706088d35e415e6eea3d3fc
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-10-29 13:07:48 +0330

    Practice with declaration/expression

---
+++ declaration/script9.js
@@ -0,0 +1,9 @@
+function showValue() {
+    const num1 = 3
+    const num2 = 4
+    const resultNumber = num1 + num2
+    alert("result:" + resultNumber)
+}
+showValue();
+showValue();
+showValue();
---
+++ expression/script10.js
@@ -0,0 +1,12 @@
+const showValue = function () {
+    const num1 = 3
+    const num2 = 4
+    resultNumber = num1 + num2
+    alert("num1 + num2=" + resultNumber)
+}
+const sayHi = function () {
+    alert("Hi :)")
+}
+showValue();
+showValue();
+sayHi();
