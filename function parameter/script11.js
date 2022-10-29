b6223b5e0da75e365d36ac7e923f5040949298f0
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-10-29 13:41:34 +0330

    Practice with function parameter

---
+++ function parameter/script11.js
@@ -0,0 +1,9 @@
+function showText(userText) {
+    alert("your parameter" + userText)
+}
+function showTwoNumber(num1, num2) {
+    alert("num1:" + num1)
+    alert("num2:" + num2)
+
+}
+showTwoNumber(15, 86);
