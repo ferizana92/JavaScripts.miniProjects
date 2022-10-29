7104f26f3fb0542b152ec44bf8358ce4df9130e5
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-10-29 14:13:27 +0330

    Practice about avarage with function

--- avarage/script3.js
+++ avarage/script3.js
@@ -1,7 +1,15 @@
 const userThirdNumber = Number(prompt("please enter third number!"));
-const userFirstNumber =Number( prompt("please enter first number!"));
+const userFirstNumber = Number(prompt("please enter first number!"));
 const userSecondNumber = Number(prompt("please enter second number!"));
 
 const Avarage = (userFirstNumber + userSecondNumber + userThirdNumber) / 3
 
 alert(Avarage)
+//==========avarage with function============
+function avarageCalculator(num1, num2, num3) {
+    const sum = num1 + num2 + num3
+    const avg = sum / 3
+    alert("Avarage is: " + avg)
+
+}
+avarageCalculator(5, 10, 15)
