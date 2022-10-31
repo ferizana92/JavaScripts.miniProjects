22055a70baa964fe07cd3137f33b112a0bed485d
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-10-31 12:26:58 +0330

    practice with strings

---
+++ string/script14.js
@@ -0,0 +1,15 @@
+const text = "I love JavaScript"
+console.log(text[0])
+console.log(text.charAt(5))
+console.log(text.charCodeAt(0))
+console.log(text + " & React")
+console.log(text.concat(" & React"))
+
+const finalText = text.concat(" & React")
+console.log(finalText)
+console.log(text.trim())
+console.log(text.toUpperCase())
+console.log(text.toLowerCase())
+console.log(text.search("Java"))
+console.log(text.indexOf("love"))
+console.log(text.includes("love"))
