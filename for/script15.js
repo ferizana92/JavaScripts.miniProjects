09d0770978c8fe6a723284e508334810108ecaa7
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-11-02 19:08:41 +0330

    practice with For3

--- for/script15.js
+++ for/script15.js
@@ -1,35 +1,48 @@
-for (var i = 0; i < 5; i++) {
-    console.log(i, "yes")
-}
-// =========
-const username = "ferizana92"
-for (var i = 0; i < username.length; i++) {
-    console.log(username[i])
-}
-// =========price of product==========//
-var allPrices = 0
-for (var i = 0; i < 5; i++) {
-    allPrices = allPrices + Number(prompt("Enter the product price,please!"))
-}
-console.log(allPrices)
-alert("All price of your basket is:" + allPrices)
-// ============even number==============//
+// for (var i = 0; i < 5; i++) {
+//     console.log(i, "yes")
+// }
+// // =========
+// const username = "ferizana92"
+// for (var i = 0; i < username.length; i++) {
+//     console.log(username[i])
+// }
+// // =========price of product==========//
+// var allPrices = 0
+// for (var i = 0; i < 5; i++) {
+//     allPrices = allPrices + Number(prompt("Enter the product price,please!"))
+// }
+// console.log(allPrices)
+// alert("All price of your basket is:" + allPrices)
+// // ============even number==============//
 
-for (var i = 0; i < 100; i += 2) {
-    console.log(i)
-}
-// ============odd number==============//
+// for (var i = 0; i < 100; i += 2) {
+//     console.log(i)
+// }
+// // ============odd number==============//
 
-for (var i = 1; i < 100; i += 2) {
-    console.log(i)
-}
-// ===================
-var userNumber = Number(prompt('enter the number.'))
-var sum = 0
+// for (var i = 1; i < 100; i += 2) {
+//     console.log(i)
+// }
+// // ===================
+// var userNumber = Number(prompt('enter the number.'))
+// var sum = 0
 
-for (var i = 0; userNumber / 10; i++) {
-    sum = sum + (userNumber % 10)
-    userNumber = Math.floor(userNumber / 10)
+// for (var i = 0; userNumber / 10; i++) {
+//     sum = sum + (userNumber % 10)
+//     userNumber = Math.floor(userNumber / 10)
 
+// }
+// console.log("sum of digits :", sum)
+// =======================
+
+var userNumber = Number(prompt('enter the number.'));
+var counter = 0;
+if (userNumber == 0) {
+    counter = 1;
+} else {
+    for (var i = 0; userNumber != 10; i++) {
+        counter++;
+        userNumber = Math.floor(userNumber / 10);
+    }
 }
-console.log("sum of digits :", sum)
+alert("sum of digits :", +counter)
